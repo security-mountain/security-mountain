08c4597007955557e7f3e49f5b8a4ac7bbabb003
bceda <109223555+bceda@users.noreply.github.com>
2022-08-15 17:44:06 +0100

    Update individual.js
    Signed-off-by: bceda <109223555+bceda@users.noreply.github.com>

--- src/pages/individual.js
+++ src/pages/individual.js
@@ -28,7 +28,7 @@ const BlogIndex = ({ data, location }) => {
           const title = post.frontmatter.title || post.fields.slug;
 
           return (
-            <li key={post.fields.slug} style={{ marginLeft: "10rem" }}>
+            <li key={post.fields.slug} style={{ marginLeft: "9rem" }}>
                 <header>
                   <button>
                     <Link to={post.fields.slug} itemProp="url">
