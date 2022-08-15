5bdd96e427c9f0152a42a6abc4734e9f1b4929ea
bceda <109223555+bceda@users.noreply.github.com>
2022-08-15 13:57:19 +0100

    Update individual.js
    Signed-off-by: bceda <109223555+bceda@users.noreply.github.com>

--- src/pages/individual.js
+++ src/pages/individual.js
@@ -29,11 +29,11 @@ const BlogIndex = ({ data, location }) => {
           return (
             <li key={post.fields.slug}>
                 <header>
-                  <Button>
+                  <button>
                     <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
-                  </Button>
+                  </button>
                 </header>
             </li>
           );
